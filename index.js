dc1862eca7de37955a16fa21b3b03a4b8c570c6f
Doven <doven.obertinca@gmail.com>
2022-04-12 16:01:39 +0200

    done

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 const video = document.getElementById("video");
 const play = document.getElementById("play");
 const stop = document.getElementById("stop");
-const progress = document.getElementById("video");
-const tiestamp = document.getElementById("timestamp");
+const progress = document.getElementById("progress");
+const timestamp = document.getElementById("timestamp");
 
 // Play & pause video
 function toggleVideoStatus() {
@@ -25,12 +25,26 @@ function updatePlayIcon() {
 
 // update progress & timestamp
 function updateProgress() {
-  return true;
+  progress.value = (video.currentTime / video.duration) * 100;
+
+  // Get minutes
+  let mins = Math.floor(video.currentTime / 60);
+  if (mins < video.duration) {
+    mins = "0" + String(mins);
+  }
+
+  // Get seconds
+  let secs = Math.floor(video.currentTime % 60);
+  if (secs < video.duration) {
+    secs = "0" + String(secs);
+  }
+
+  timestamp.innerHTML = `${mins}:${secs}`;
 }
 
-// Set video ime to progress
+// Set video time to progress
 function setVideoProgress() {
-  return true;
+  video.currentTime = (+progress.value * video.duration) / 100;
 }
 
 // Stop video
